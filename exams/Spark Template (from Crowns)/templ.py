4af45a3763a35bf65b875dfc830c0c49a647294c
Diana Husanu <dhusanu0@gmail.com>
2025-02-18 12:21:23 +0100

    templates from respondus platform used during exam

---
+++ exams/Spark Template (from Crowns)/templ.py
@@ -0,0 +1,15 @@
+import pyspark
+
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+sc = spark.sparkContext
+
+
+# Part 1
+
+
+
+# Part 2
+
